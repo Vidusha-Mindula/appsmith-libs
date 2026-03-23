99c03306b6b0558be37fe69252f2b9849edc3c26
Vidusha Mindula <148189028+Vidusha-Mindula@users.noreply.github.com>
2026-03-23 17:32:09 +0530

    Update Buttonlib.js

--- Buttonlib.js
+++ Buttonlib.js
@@ -1,21 +1,29 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' 
-    ? module.exports = factory() :
-  typeof define === 'function' && define.amd 
-    ? define(factory) :
-  (global.ButtonColors = factory());
+	typeof exports === 'object' && typeof module !== 'undefined' 
+		? module.exports = factory() :
+	typeof define === 'function' && define.amd 
+		? define(factory) :
+	(global.ButtonColors = factory());
 }(this, function () {
-  return {
-    get_create_btn_color() {
-      return "#1E90FF";
-    },
-    
-    get_filter_btn_color() {
-      return "#4682B4";
-    },
-    
-    get_reset_btn_color() {
-      return "#B0C4DE";
-    }
-  };
+	return {
+		get_create_btn_color() {
+			return "#1E90FF";
+		},
+		
+		get_filter_btn_color() {
+			return "#4682B4";
+		},
+		
+		get_reset_btn_color() {
+			return "#B0C4DE";
+		},
+		
+		get_inactive_color() {
+			return "#E6F0FA";
+		},
+		
+		get_active_color() {
+			return "#1E90FF";
+		}
+	};
 }));
